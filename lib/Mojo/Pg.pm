404a645f0072020f68b1de250875f4c5e7325d67
Sebastian Riedel <sri@cpan.org>
2014-10-06 01:25:47 +0200

    bump version

--- lib/Mojo/Pg.pm
+++ lib/Mojo/Pg.pm
@@ -11,7 +11,7 @@ has max_connections => 5;
 has options => sub { {AutoCommit => 1, PrintError => 0, RaiseError => 1} };
 has [qw(password username)] => '';
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub db {
   my $self = shift;
