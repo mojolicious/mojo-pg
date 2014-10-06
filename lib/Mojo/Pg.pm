c9ad5c1421a11d34339e8093837dd222e6e961bf
Sebastian Riedel <sri@cpan.org>
2014-10-06 22:02:53 +0200

    use the term connection

--- lib/Mojo/Pg.pm
+++ lib/Mojo/Pg.pm
@@ -186,8 +186,8 @@ following new ones.
 
   my $db = $pg->db;
 
-Get L<Mojo::Pg::Database> object for a cached or newly created database
-handle. The database handle will be automatically cached again when that
+Get L<Mojo::Pg::Database> object for a cached or newly established database
+connection. The database handle will be automatically cached again when that
 object is destroyed, so you can handle connection timeouts gracefully by
 holding on to it only for short amounts of time.
 
