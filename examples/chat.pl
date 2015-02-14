104170b5f026b66e5ee69de82176c3e9337dd333
Sebastian Riedel <sri@cpan.org>
2015-02-14 16:57:07 +0100

    add pub/sub example application

---
+++ examples/chat.pl
@@ -0,0 +1,38 @@
+use Mojolicious::Lite;
+use Mojo::Pg;
+
+helper pg => sub { state $pg = Mojo::Pg->new('postgresql://postgres@/test') };
+
+get '/' => 'chat';
+
+websocket '/channel' => sub {
+  my $c = shift;
+
+  $c->inactivity_timeout(3600);
+
+  # Forward messages from the browser to PostgreSQL
+  $c->on(message => sub { shift->pg->pubsub->notify(message => shift) });
+
+  # Forward messages from PostgreSQL to the browser
+  my $cb = $c->pg->pubsub->listen(message => sub {
+    my ($pubsub, $msg) = @_;
+    $c->send($msg);
+  });
+  $c->on(finish => sub { shift->pg->pubsub->unlisten(message => $cb) });
+};
+
+app->start;
+
+__DATA__
+
+@@ chat.html.ep
+
+<form onsubmit="sendChat(this.children[0]); return false"><input></form>
+<div id="log"></div>
+<script>
+  var ws  = new WebSocket('<%= url_for('channel')->to_abs %>');
+  ws.onmessage = function (e) {
+    document.getElementById('log').innerHTML += '<p>' + e.data + '</p>';
+  };
+  function sendChat(input) { ws.send(input.value); input.value = '' }
+</script>
