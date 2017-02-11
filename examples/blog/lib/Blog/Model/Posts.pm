0111533853513d77b248d0e7df8f8c74f7caa327
Sebastian Riedel <sri@cpan.org>
2017-02-11 16:05:37 +0100

    use the new CRUD method in the blog example

--- examples/blog/lib/Blog/Model/Posts.pm
+++ examples/blog/lib/Blog/Model/Posts.pm
@@ -5,23 +5,24 @@ has 'pg';
 
 sub add {
   my ($self, $post) = @_;
-  my $sql = 'insert into posts (title, body) values (?, ?) returning id';
-  return $self->pg->db->query($sql, $post->{title}, $post->{body})->hash->{id};
+  return $self->pg->db->insert('posts', $post, {returning => 'id'})->hash->{id};
 }
 
-sub all { shift->pg->db->query('select * from posts')->hashes->to_array }
+sub all { shift->pg->db->select('posts')->hashes->to_array }
 
 sub find {
   my ($self, $id) = @_;
-  return $self->pg->db->query('select * from posts where id = ?', $id)->hash;
+  return shift->pg->db->select('posts', undef, {id => $id})->hash;
 }
 
-sub remove { shift->pg->db->query('delete from posts where id = ?', shift) }
+sub remove {
+  my ($self, $id) = @_;
+  $self->pg->db->delete('posts', {id => $id});
+}
 
 sub save {
   my ($self, $id, $post) = @_;
-  my $sql = 'update posts set title = ?, body = ? where id = ?';
-  $self->pg->db->query($sql, $post->{title}, $post->{body}, $id);
+  $self->pg->db->update('posts', $post, {id => $id});
 }
 
 1;
