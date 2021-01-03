5291b43627b284f8891cbe692e51d1882eb940b8
Sebastian Riedel <sri@cpan.org>
2021-01-04 00:03:58 +0100

    Bump version

--- lib/Mojo/Pg.pm
+++ lib/Mojo/Pg.pm
@@ -528,7 +528,7 @@ William Lindley
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2014-2020, Sebastian Riedel and others.
+Copyright (C) 2014-2021, Sebastian Riedel and others.
 
 This program is free software, you can redistribute it and/or modify it under the terms of the Artistic License version
 2.0.
